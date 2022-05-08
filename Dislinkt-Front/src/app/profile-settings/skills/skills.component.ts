a611f9310a497f16f85f033ee9c5f2f0a762b135
anastasijapopovic <popovicanastasija@gmail.com>
2022-05-08 11:50:33 +0200

    add new skill and category modal dialog

--- Dislinkt-Front/src/app/profile-settings/skills/skills.component.ts
+++ Dislinkt-Front/src/app/profile-settings/skills/skills.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-skills',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
-
-  constructor() { }
+  isIconPress: boolean = false;
+  addForm: FormGroup;
+  addSkillForm: FormGroup;
+  @ViewChild('addSkillCategory') addDialog!: TemplateRef<any>;
+  @ViewChild('addSkill') addSkillDialog!: TemplateRef<any>;
+  categories: string[] = ['Technical skills', 'SOft skills'];
+  constructor(private formBuilder: FormBuilder, public dialog: MatDialog) {
+    this.addForm = this.formBuilder.group({
+      category: [''],
+    });
+    this.addSkillForm = this.formBuilder.group({
+      skill: [''],
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  opetAddCategoryDialog(event: any) {
+    event?.stopPropagation();
+    const myTempDialog = this.dialog.open(this.addDialog);
+    myTempDialog.afterClosed().subscribe((res) => {
+      console.log({ res });
+    });
+    this.isIconPress = false;
+  }
+
+  opetAddSkillDialog(event: any) {
+    event?.stopPropagation();
+    const myTempDialog = this.dialog.open(this.addSkillDialog);
+    myTempDialog.afterClosed().subscribe((res) => {
+      console.log({ res });
+    });
+    this.isIconPress = false;
   }
 
 }
