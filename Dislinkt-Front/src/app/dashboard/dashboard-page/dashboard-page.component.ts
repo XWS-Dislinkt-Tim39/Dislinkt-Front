7522401e7d611e1c91bdf609382494265c7b4906
KNatali <natatsi24@gmail.com>
2022-05-25 22:50:59 +0200

    add reactions buttons style

--- Dislinkt-Front/src/app/dashboard/dashboard-page/dashboard-page.component.ts
+++ Dislinkt-Front/src/app/dashboard/dashboard-page/dashboard-page.component.ts
@@ -8,8 +8,18 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./dashboard-page.component.scss']
 })
 export class DashboardPageComponent implements OnInit {
-  dilogRef: any
-  constructor(public dialog: MatDialog) { }
+  dilogRef: any;
+  isLike:boolean=false;
+  isDislike:boolean=false;
+  isComment:boolean=false;
+  likeStyle:string='';
+  dislikeStyle:string='';
+  commentStyle:string='';
+  constructor(public dialog: MatDialog) {
+    this. likeStyle = 'reaction-button';
+    this. dislikeStyle = 'reaction-button';
+    this. commentStyle = 'reaction-button';
+   }
 
   ngOnInit(): void {
   }
@@ -19,6 +29,33 @@ export class DashboardPageComponent implements OnInit {
       data: {
       }
     });
+  }
+  toggleLike(){
+    this.isLike=!this.isLike;
+    if(this.isLike){
+      this.likeStyle='like-button';
+    }
+    else{
+      this.likeStyle = 'reaction-button';
+    }
+  }
+  toggleDislike(){
+    this.isDislike=!this.isDislike;
+    if(this.isDislike){
+      this.dislikeStyle='dislike-button';
+    }
+    else{
+      this.dislikeStyle = 'reaction-button';
+    }
+  }
+  toggleComment(){
+    this.isComment=!this.isComment;
+    if(this.isComment){
+      this.commentStyle='comment-button';
+    }
+    else{
+      this.commentStyle = 'reaction-button';
+    }
   }
 
 }
