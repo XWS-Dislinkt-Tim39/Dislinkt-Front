d1827a6cf854d4a17ae4bde1185c3eae8de9594c
KNatali <natatsi24@gmail.com>
2022-05-29 11:03:03 +0200

    added edit experience modal dialog

--- Dislinkt-Front/src/app/profile-settings/experience/experience.component.ts
+++ Dislinkt-Front/src/app/profile-settings/experience/experience.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { UserExperience } from 'src/app/core/models/user-experience.model';
+import { User } from 'src/app/core/models/user.model';
 import { JwtService } from 'src/app/core/services/jwt.service';
 import { ProfileService } from 'src/app/core/services/profile.service';
 import { UserExperienceComponent } from 'src/app/dashboard/user-experience/user-experience.component';
@@ -14,8 +15,9 @@ import { UserExperienceComponent } from 'src/app/dashboard/user-experience/user-
 export class ExperienceComponent implements OnInit {
   isIconPress: boolean = false;
   addForm: FormGroup;
+  editForm: FormGroup;
   userId: any;
-  experience: UserExperience = {
+  selectedExperience: UserExperience = {
     userId: "",
     nameOfCompany: "",
     fieldOfWork: "",
@@ -23,11 +25,22 @@ export class ExperienceComponent implements OnInit {
     endDate: new Date(),
     description: ""
   }
+  experience: UserExperience = {
+    userId: "",
+    nameOfCompany: "",
+    fieldOfWork: "aaa",
+    startDate: new Date(),
+    endDate: new Date(),
+    description: ""
+  }
   experiences:any=[]
   startDate: Date = new Date();
   endDate: Date = new Date();
+  startDateEdit: Date = new Date();
+  endDateEdit: Date = new Date();
 
   @ViewChild('addExperience') addDialog!: TemplateRef<any>;
+  @ViewChild('editExperience') editDialog!: TemplateRef<any>;
 
   constructor(private formBuilder: FormBuilder,
     public dialog: MatDialog,
@@ -42,6 +55,14 @@ export class ExperienceComponent implements OnInit {
       endDate: ['', Validators.required],
       description: ['']
     });
+
+    this.editForm = this.formBuilder.group({
+      nameOfCompany: ['', Validators.required],
+      fieldOfWork: ['', Validators.required],
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+      description: ['']
+    });
   }
 
   ngOnInit(): void {
@@ -49,25 +70,42 @@ export class ExperienceComponent implements OnInit {
     this.getAllExperiences();
   }
   get addFormControl(): { [key: string]: AbstractControl; } { return this.addForm.controls; }
+  get editFormControl(): { [key: string]: AbstractControl; } { return this.addForm.controls; }
+
   opetAddDialog(event: any) {
     event?.stopPropagation();
     const myTempDialog = this.dialog.open(this.addDialog);
     myTempDialog.afterClosed().subscribe((res) => {
       console.log({ res });
     });
-    this.isIconPress = false;
+  }
+  opetEditDialog(event: any,row:UserExperience) {
+    event?.stopPropagation();
+    this.selectedExperience=row;
+    const myTempDialog = this.dialog.open(this.editDialog);
+    this.setEditFields();
+    myTempDialog.afterClosed().subscribe((res) => {
+      console.log({ res });
+    });
   }
 
   getAllExperiences() {
     this.profileService.getAboutInfo(this.userId).subscribe(data => {
       this.experiences=data.workExperiences;
-      alert(this.experiences[3].nameOfCompany)
     }, error => {
       console.log(error.error);
       alert('Error! Try again');
     });
   }
 
+
+  setEditFields(){
+    this.editForm.get('nameOfCompany')?.setValue(this.selectedExperience.nameOfCompany);
+    this.editForm.get('fieldOfWork')?.setValue(this.selectedExperience.fieldOfWork);
+    this.startDateEdit=this.selectedExperience.startDate;
+    this.endDateEdit=this.selectedExperience.endDate;
+    this.editForm.get('description')?.setValue(this.selectedExperience.description);
+  }
   addWorkExperience() {
     if (this.addForm.invalid) {
       return;
@@ -79,7 +117,8 @@ export class ExperienceComponent implements OnInit {
     this.experience.endDate = this.addForm.value.endDate;
     this.experience.description = this.addForm.value.description;
     this.profileService.addWorkExperience(this.experience).subscribe(data => {
-      alert('Sucessfully added new work experience')
+      alert('Sucessfully added new work experience');
+      window.location.reload();
     }, error => {
       console.log(error.error);
       alert('Error! Try again');
