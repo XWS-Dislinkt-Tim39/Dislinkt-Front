8abad6e7d043aa519906308ebcc3766121168ea4
KNatali <natatsi24@gmail.com>
2022-05-31 13:54:04 +0200

    add public profile education list

--- Dislinkt-Front/src/app/public/profile-education/profile-education.component.ts
+++ Dislinkt-Front/src/app/public/profile-education/profile-education.component.ts
@@ -8,13 +8,19 @@ import { Router } from '@angular/router';
 })
 export class ProfileEducationComponent implements OnInit {
   routeState: any;
-  selectedProfile:any
+  selectedProfile:any;
+  educations:any;
   constructor( private router: Router) { 
     this.routeState = this.router.getCurrentNavigation()?.extras.state;
     this.selectedProfile = this.routeState;
   }
 
   ngOnInit(): void {
+    this.getEducation()
   }
+  getEducation(){
+    this.educations=this.selectedProfile.educations;
+  }
+  
 
 }
