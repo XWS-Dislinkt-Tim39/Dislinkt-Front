ecf3da35de5ff7b5402fa9242a337d81bdf4c0f2
anastasijapopovic <popovic.in46.2018@uns.ac.rs>
2022-06-02 14:48:39 +0200

    Show posts for public profiles

--- Dislinkt-Front/src/app/public/profile-posts/profile-posts.component.ts
+++ Dislinkt-Front/src/app/public/profile-posts/profile-posts.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PostService } from 'src/app/core/services/post.service';
+import { ProfileService } from 'src/app/core/services/profile.service';
 
 @Component({
   selector: 'app-profile-posts',
@@ -8,13 +10,60 @@ import { Router } from '@angular/router';
 })
 export class ProfilePostsComponent implements OnInit {
   routeState: any;
-  selectedProfile:any
-  constructor( private router: Router) { 
+  selectedProfile:any;
+  posts:any[]=[];
+  sortedPosts:any[]=[];
+  commentStyle: string = '';
+  constructor( private router: Router, private postService: PostService, private profileService:ProfileService) { 
     this.routeState = this.router.getCurrentNavigation()?.extras.state;
     this.selectedProfile = this.routeState;
+    this.commentStyle = 'reaction-button';
   }
 
   ngOnInit(): void {
+    this.getPosts();
   }
 
+  getPosts(){
+    this.postService.getUserPosts(this.selectedProfile.id).subscribe(data=>{
+      this.posts=data;
+      this.posts.forEach((value,i: any)=>{
+        value.showComments=false;
+        value.newCommentText='';
+        //this.getCommentUser(value);
+    });
+    this.sortedPosts = this.posts.sort(
+      (objA, objB) => new Date(objB.dateTimeOfPublishing).getTime() - new Date(objA.dateTimeOfPublishing).getTime(),
+    );
+    },error=>{
+      alert('Error! Try again!')
+    })
+  }
+
+  /*getCommentUser(post:any){
+    post.comments.forEach((value:any,i: any)=>{
+      this.profileService.getAboutInfo(value.userId).subscribe(data=>{
+        value.userFirstName=data.firstName;
+        value.userLastName=data.lastName;
+      })
+  });
+  }*/
+
+  isLiked(index:any):boolean{
+    if(this.posts[index].likes.indexOf(this.selectedProfile.id) !== -1) {
+      return true
+    }
+    return false
+  }
+
+  isDisliked(index:any):boolean{
+    if(this.posts[index].dislikes.indexOf(this.selectedProfile.id) !== -1) {
+      return true
+    }
+    return false
+  }
+
+  showComments(index:any){
+    this.posts[index].showComments=!this.posts[index].showComments;
+  }
 }
