ba155de52f35c81db5682b7f2ba1dde655cd984a
anastasijapopovic <popovic.in46.2018@uns.ac.rs>
2022-07-10 15:47:48 +0200

    Add real registrations data to chart

--- Dislinkt-Front/src/app/profile-settings/connections-chart/connections-chart.component.ts
+++ Dislinkt-Front/src/app/profile-settings/connections-chart/connections-chart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart, registerables} from 'node_modules/chart.js';
+import { ProfileService } from 'src/app/core/services/profile.service';
 
 Chart.register(...registerables);
 
@@ -9,58 +10,82 @@ Chart.register(...registerables);
   styleUrls: ['./connections-chart.component.scss']
 })
 export class ConnectionsChartComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-    const myChart = new Chart("connectionsChart", {
-      type: 'bar',
-      data: {
-          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-          datasets: [{
-              label: 'Number of connections for every month',
-              data: [12, 19, 3, 5, 2, 3, 30, 50, 15, 24, 38, 14],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              y: {
-                  beginAtZero: true
-                  
-              }
-          },
-          plugins: {
-            title: {
-                display: true,
-                text: 'Number of connections for every month',
-                padding: {
-                    top: 10,
-                    bottom: 30
+    registrations:any=[];
+    months:number[]=[0,0,0,0,0,0,0,0,0,0,0,0];  
+    constructor(private profileService: ProfileService) { }
+    delay(ms: number) {
+        return new Promise( resolve => setTimeout(resolve, ms) );
+    }
+    async ngOnInit(): Promise<void> {
+        this.getAllActivities();
+        await this.delay(200);
+        this.getMonthsValues();
+        await this.delay(200);
+        const myChart = new Chart("connectionsChart", {
+        type: 'bar',
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            datasets: [{
+                data:this.months,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
                 }
             },
-            legend: {
-                display: false
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Number of registrations for every month',
+                    padding: {
+                        top: 10,
+                        bottom: 30
+                    }
+                },
+                legend: {
+                    display: false
+                }
             }
         }
-      }
-  });
-  }
+    });
+    
+    }
+
+    getAllActivities(){
+        this.profileService.getAllActivities().subscribe(data=>{
+        data.forEach((element :any)=> {
+            if(element.type==0){
+                this.registrations.push(element);
+                console.log(this.registrations)
+            }
+        });
+        })
+    }
+
+    getMonthsValues(){
+        this.registrations.forEach((element: any) => {
+            let i=new Date(element.date).getMonth();
+            this.months[i]=this.months[i]+1;
+        });
+    }
 
 }
