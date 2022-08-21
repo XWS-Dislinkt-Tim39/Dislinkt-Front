d3e3611557d62ef902aab8c578c5fe3bc900bdae
Natalija Krsmanović <64694174+KNatali@users.noreply.github.com>
2022-08-21 22:47:11 +0200

    fix add post style

--- Dislinkt-Front/src/app/dashboard/add-post/add-post.component.ts
+++ Dislinkt-Front/src/app/dashboard/add-post/add-post.component.ts
@@ -17,11 +17,6 @@ export class AddPostComponent implements OnInit {
   form: FormGroup;
   @ViewChild('UploadFileInput') uploadFileInput: ElementRef | undefined;
   myfilename = 'Select File';
-  showText: boolean = false;
-  showImage: boolean = false;
-  showLink: boolean = false;
-  textStyle: string = '';
-  imageStyle: string = '';
   imageDropifyStyle: string = '';
   newPost: NewPost = {
     userId: '',
@@ -39,8 +34,6 @@ export class AddPostComponent implements OnInit {
     private connectionService: ConnectionService
   ) {
     this.userId = this.jwtService.getUserId();
-    this.textStyle = 'text-default';
-    this.imageStyle = 'text-default';
     this.imageDropifyStyle = 'image-default';
     this.form = this.formBuilder.group({
       text: [''],
@@ -66,16 +59,6 @@ export class AddPostComponent implements OnInit {
     reader.readAsDataURL(file);
   }
 
-  toggleText() {
-    this.showText = !this.showText;
-    if (this.showText) {
-      this.textStyle = 'text-checked';
-    }
-    else {
-      this.textStyle = 'text-default';
-    }
-  }
-
   getMyConnections() {
     this.connectionService.getConnections(this.userId).subscribe(data => {
       if (data != null) {
@@ -84,48 +67,23 @@ export class AddPostComponent implements OnInit {
     })
   }
 
-  toggleImage() {
-    this.showImage = !this.showImage;
-    if (this.showImage) {
-      this.imageDropifyStyle = 'image-checked';
-    }
-    else {
-      this.imageDropifyStyle = 'image-default';
-    }
-    if (this.showImage) {
-      this.imageStyle = 'text-checked';
-    }
-    else {
-      this.imageStyle = 'text-default';
-    }
-  }
-
   addPost() {
     this.newPost.userId = this.userId;
-    if (this.showText) {
-      this.newPost.text = this.form.value.text;
-    }
+    this.newPost.text = this.form.value.text;
     this.newPost.dateTimeOfPublishing = new Date();
     this.newPost.followersId = this.connections;
-    // this.newPost.image=this.imageData!;
     this.postService.addNewPost(this.newPost).subscribe(data => {
-      alert('Sucessfully added new post');
       console.log(data)
       this.addImage(data.id);
-      //window.location.reload();
     }, error => {
       alert('Error! Try again!')
     })
   }
 
-
   onFileSelected(event: any) {
-
     const file: File = event.target.files[0];
     console.log(file);
-
     if (file) {
-
       this.fileName = file.name;
       console.log(this.fileName);
       const formData = new FormData();
@@ -134,11 +92,14 @@ export class AddPostComponent implements OnInit {
     }
   }
 
-  addImage(postId:number){
+  addImage(postId: number) {
     console.log('ss')
-   this.postService.saveImage(this.imageData,postId).subscribe(data=>{
-    alert('uhuu')
-   })
+    this.postService.saveImage(this.imageData, postId).subscribe(data => {
+      alert('Sucessfully added new post');
+      window.location.reload();
+    }, error => {
+      alert('Error!Try again!');
+    })
   }
 
 
